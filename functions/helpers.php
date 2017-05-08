7db0a12c9bb870e49a001b780846c718000a2c60
Harry Pujols <harrypujols@me.com>
2017-05-08 13:41:31 -0400

    Inline sag helper

---
+++ functions/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action( 'wp_loaded', 'inline_svg', 1 );
+function inline_svg($file = null)
+{
+  $icon_path =  get_template_directory()  . '/static/icons/' . $file;
+  if (file_exists($icon_path)) {
+    $svg = file_get_contents($icon_path);
+    $svg = preg_replace('!^[^>]+>(\r\n|\n)!', '', $svg);
+    $svg = preg_replace('~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\s*~i', '', $svg);
+    echo $svg;
+  }
+}
