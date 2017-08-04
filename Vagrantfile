9e7af1dfc15990c87bccd5aa1fb2e78daa631121
Harry Pujols <harrypujols@me.com>
2017-08-04 11:55:00 -0400

    Clean up

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,6 @@ PORT='8000'
 Vagrant.configure('2') do |config|
   config.vm.box = 'bento/ubuntu-16.04'
   config.vm.network 'forwarded_port', guest: 80, host: PORT
-  config.vm.network :forwarded_port, guest: 22, host: 2200, id: 'ssh', auto_correct: true
   config.vm.synced_folder './src', '/home/vagrant/shared', :mount_options => ['dmode=777','fmode=666']
   config.vm.provision :shell, path: 'provision.sh', args: PORT
 end
--- provision.sh
+++ provision.sh
@@ -6,29 +6,29 @@ PROJECT='wordpress'
 PORT=$1
 
 # update / upgrade
-sudo apt-get update
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 # install php latest
-sudo apt-get install -y php libapache2
+apt-get install -y php libapache2
 
 # install composer
-sudo apt-get install -y zip unzip composer
+apt-get install -y zip unzip composer
 
 # install mysql and give password to installer
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server php-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server php-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
-sudo phpenmod mcrypt
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+apt-get -y install phpmyadmin
+phpenmod mcrypt
 
 # setup mysql user
 MY=$(cat <<EOF
@@ -38,19 +38,19 @@ password=root
 host=localhost
 EOF
 )
-echo "${MY}" > /home/vagrant/.my.cnf
+echo "$MY" > /home/vagrant/.my.cnf
 
 # create a database
-mysql --user=$PASSWORD --password=$PASSWORD -e "create database ${PROJECT};"
+mysql --user=$PASSWORD --password=$PASSWORD -e "create database $PROJECT;"
 
 # install apache
-sudo apt-get install -y apache2
+apt-get install -y apache2
 
 # enable mods
-sudo a2enmod rewrite
-sudo a2enmod headers
-sudo a2enmod expires
-sudo a2enmod include
+a2enmod rewrite
+a2enmod headers
+a2enmod expires
+a2enmod include
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -67,32 +67,32 @@ VHOST=$(cat <<EOF
 EOF
 )
 
-echo "${VHOST}" > /etc/apache2/sites-available/$PROJECT.local.conf
-sudo a2ensite $PROJECT.local.conf
+echo "$VHOST" > /etc/apache2/sites-available/$PROJECT.local.conf
+a2ensite $PROJECT.local.conf
 
 # change apache configurations
-sudo sed -i "/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/" /etc/apache2/apache2.conf
+sed -i "/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/" /etc/apache2/apache2.conf
 
 # install git
-sudo apt-get -y install git
+apt-get -y install git
 
 # install sendmail
-sudo apt-get install -y sendmail
+apt-get install -y sendmail
 
 # installing wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 
 # installing wordpress
-sudo rm /var/www/html/index.html
-sudo chmod -R 777 /var/www
+rm /var/www/html/index.html
+chmod -R 777 /var/www
 cd /var/www/html/
 wp core download --allow-root
 wp config create --dbname=$PROJECT --dbuser=$PASSWORD --dbpass=$PASSWORD --allow-root
 wp core install --url=localhost:$PORT --title=wp-starter --admin_user=admin --admin_password=password --admin_email=webmaster@localhost.local --allow-root
 wp plugin install timber-library --allow-root
-wp rewrite structure /%postname%/ --allow-root
+wp rewrite structure /%postname%/ --hard --allow-root
 
 # installing theme
 ln -s /home/vagrant/shared/ /var/www/html/wp-content/themes/wp-starter
@@ -100,13 +100,13 @@ wp theme activate wp-starter --allow-root
 wp plugin activate timber-library --allow-root
 
 # passing over the permissions
-sudo adduser vagrant www-data
-sudo chown -R www-data:www-data /var/www
-sudo chmod -R g+rwX /var/www
+adduser vagrant www-data
+chown -R www-data:www-data /var/www
+chmod -R g+rwX /var/www
 
 # restart apache
-sudo service apache2 restart
+service apache2 restart
 
 # all done
-echo "${PROJECT} username is \"admin\" password is \"password\""
-printf "\033[0;36m${PROJECT} site running on \033[0;35mhttp://localhost:${PORT}\033[0m"
+echo "$PROJECT username is \"admin\" password is \"password\""
+printf "\033[0;36m${PROJECT} site running on \033[0;35mhttp://localhost:$PORT\033[0m"
